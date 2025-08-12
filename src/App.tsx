41a52dd9daa6836f0beea503a433e6491ddc91e5
david cubrilla <33374798+davcub@users.noreply.github.com>
2025-08-12 16:29:46 -0400

    [fix file export] - fix

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ContactPage from './pages/ContactPage';
-import ServicePage from './components/servicePage';
+import ServicePage from './components/ServicePage';
 
 function App(): JSX.Element {
 	return (
